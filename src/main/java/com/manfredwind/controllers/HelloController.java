6e102a6de1a9b5ed8649032496361e7ad4a9d10c
reachnileshvaity1 <76544355+reachnileshvaity1@users.noreply.github.com>
2022-05-16 12:59:26 +0530

    Update HelloController.java

--- src/main/java/com/manfredwind/controllers/HelloController.java
+++ src/main/java/com/manfredwind/controllers/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 	
 	@GetMapping("/")
 	public String hello() {
-		return "Hello!";
+		return "Welcome to site developed using Spring boot with Maven for Softmax Solutions, a software company";
 	}
 
 }
